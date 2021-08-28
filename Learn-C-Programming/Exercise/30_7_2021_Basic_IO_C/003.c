f28847084e11a7b5aa42e0ce60215b5f0ff636bc
Akkarapon Phikulsri <lacakkarapon@gmail.com>
2021-08-28 09:10:55 +0700

    Update

--- Learn-C-Programming/Exercise/30_7_2021_Basic_IO_C/003.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	float price=2500;
-	
-	float result = ( price * 7 ) / 100; 
-	printf("Your price is %.3f", result);
-	return 0;
-}
--- Learn-C-Programming/README.md
+++
@@ -1,6 +0,0 @@
-# Learn-C-programming
-learn C programming coding flows
-
-Workshop or exercise or testing and hobbies
-
-
