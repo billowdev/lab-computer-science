42e7da0ad0a915d5d59a4daaf0325d3c2e2227dd
Akkarapon Phikulsri <lacakkarapon@gmail.com>
2021-08-28 09:04:31 +0700

    Week6
    Lab and Exercise from C# Advance Programming Class - 10 Aug. 2021

---
+++ Advance-Programming/Week6/DemoDoWhileLoop/DemoDoWhileLoop/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoDoWhileLoop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDoWhile_Click(object sender, EventArgs e)
+        {
+            int i;
+            i = 20;
+            lstShow.Items.Clear();
+            do
+            {
+                lstShow.Items.Add("วนลูปครั้งที่ " + i);
+                i--;
+            } while (i > 0);
+        }
+    }
+}
