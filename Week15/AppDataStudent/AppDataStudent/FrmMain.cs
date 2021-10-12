ff5941321cf81687339bcc2e222d0c8b32c3a7bc
lacakp <lacakkarapon@gmail.com>
2021-10-12 21:36:08 +0700

    connect database

--- Week15/AppDataStudent/AppDataStudent/FrmMain.cs
+++ Week15/AppDataStudent/AppDataStudent/FrmMain.cs
@@ -33,27 +33,33 @@ namespace AppDataStudent
                 Conn.Close();
             }
             Conn.ConnectionString = strConn;
+            Conn.Open();
+
+            ShowAllStudent();
         }
 
         private void ShowAllStudent()
         {
-            string sqlStu = "SELECT * FROM tb_hisstudent";
+            string sqlStu = "select * from tb_hisstudent";
             if (IsFind == true)
             {
                 ds.Tables["tb_hisstudent"].Clear();
-                da = new OleDbDataAdapter(sqlStu, Conn);
-                da.Fill(ds, "tb_hisstudent");
-                if (ds.Tables["tb_hisstudent"].Rows.Count != 0)
-                {
-                    IsFind = true;
-                    dgvAllStduent.ReadOnly = true;
-                    dgvAllStduent.DataSource = ds.Tables["tb_hisstudent"];
-                }
-                else
-                {
-                    IsFind = false;
-                }
             }
+
+            da = new OleDbDataAdapter(sqlStu, Conn);
+            da.Fill(ds, "tb_hisstudent");
+
+            if (ds.Tables["tb_hisstudent"].Rows.Count != 0)
+            {
+                IsFind = true;
+                dgvAllStduent.ReadOnly = true;
+                dgvAllStduent.DataSource = ds.Tables["tb_hisstudent"];
+            }
+            else
+            {
+                IsFind = false;
+            }
+            
         }
     }
 }
